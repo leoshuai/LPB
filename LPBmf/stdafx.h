e9b0866f2c10151479cefe4105f252ee07957789
leoHomeNew\Leo <shuai0liu@gmail.com>
2018-02-19 22:29:21 -0300

    Built

---
+++ LPBmf/stdafx.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "targetver.h"
+#include <stdio.h>
+#include <tchar.h>
+
+//needed by LPB
+#include <cstdlib>
+#include <Eigen/Dense>
+#include <vector>
+#include <random>
+#include <string>
+#include <iterator>
+#include <iostream>
+#include <ostream>
+#include <ilcplex/ilocplex.h>
+#include <ilconcert/iloexpression.h>
+#include "LPB.h"
+#include "InFunctions.h"
+#include "utilities.h"
+// not needed
+//#include <cassert>
+//
+//
+//#include <algorithm>
